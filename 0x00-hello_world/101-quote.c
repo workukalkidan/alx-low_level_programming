7c93e899e14fe213f071c434c6f79d3e0b2f8a90
workukalkidan <workukalkidanayele@gmail.com>
2022-04-09 07:38:27 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
 * main - Entry point
+*
 * Return: Always 1 (Success)
 */
-int main(void)
+Int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
