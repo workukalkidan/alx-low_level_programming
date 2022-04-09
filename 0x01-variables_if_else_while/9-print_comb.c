0d5f315f0b12204da6a6a9e556fb6f44bd280da8
workukalkidan <workukalkidanayele@gmail.com>
2022-04-09 10:35:53 -0700

    PRINT-comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /**
  * main - Prints all possible combinations of single-digit numbers.
  *
@@ -13,8 +11,9 @@ int main(void)
 	{
 		putchar((num % 10) + '0');
 		if (num == 9)
+		{
 			continue;
-		
+		}
 		putchar(',');
 		putchar(' ');
 	}
