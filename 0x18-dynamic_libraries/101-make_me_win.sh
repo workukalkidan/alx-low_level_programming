2d91b6d26a3ef5073277a6e88ad43e3ddff21a50
workukalkidan <workukalkidanayele@gmail.com>
2023-01-16 18:27:54 +0300

    task

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/svelezg/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
+wget https://github.com/workukalkidan/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
 export LD_PRELOAD=../inject.so
